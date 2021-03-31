acd275f93d3c9254e9ce64de268f053dcdcdaef8
Jett Thompson <jett.thompson.59@gmail.com>
2021-04-01 08:41:31 +1100

    added a check around the kernel.c unsafe pointer casts to ensure that there are no unexpected bugs on other memory models

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,6 @@
 #include <memory/pointers.h>
 
+#ifdef __x86_64
 void * get_pointer (size_t address)
 {
     return (void *) address;
@@ -8,3 +9,4 @@ size_t get_linear_address (void * ptr)
 {
     return (size_t)ptr;
 }
+#endif
