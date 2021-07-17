d207b49f1c6255974662ec48753eac5dd16b4823
Jett Thompson <jett.thompson.59@gmail.com>
2021-07-18 08:16:53 +1000

    added caps lock support and also created a data structure for storing the control key states

--- kernel/drivers/keyboard/keyboard_handler.c
+++ kernel/drivers/keyboard/keyboard_handler.c
@@ -2,18 +2,25 @@
 #include <keyboard/keycodes.h>
 #include <drivers/keyboard/handler.h>
 #include <keyboard/control_codes.h>
+#include <keyboard/status.h>
 
-static u8_t shift_status = 0;
+static keyboard_status status;
 
 void handle_key_event (key_code code, u8_t action)
 {
     if (IS_CONTROL_CODE(code)) {
         if (code == KEYCODE_SHIFT) {
-            shift_status = action;
+            status.shift_status = action;
+        }else if (code == KEYCODE_CONTROL) {
+            status.control_status = action;
+        }else if (code == KEYCODE_ALT) {
+            status.alt_status = action;
+        }else if (code == KEYCODE_CAPS_LOCK) {
+            status.caps_lock_status ^= action;
         }
     }else {
             if (action) {
-            u32_t key_char = decode_key_code(code, shift_status);
+            u32_t key_char = decode_key_code(code, status.shift_status ^ status.caps_lock_status);
             if (key_char == '\t') {
                 putchar(' ');
                 putchar(' ');
