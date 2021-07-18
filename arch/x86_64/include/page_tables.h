9847377154aa7488509b2a06391571d310a9672e
Jett Thompson <jett.thompson.59@gmail.com>
2021-07-19 05:57:40 +1000

    added a function to map a physical address as uncacheable

--- arch/x86_64/include/page_tables.h
+++ arch/x86_64/include/page_tables.h
@@ -10,7 +10,8 @@ typedef u64_t page_table_t [512] __attribute__ ((aligned (4096)));
 #define PAGE_PRESENT (1 << 0)
 #define PAGE_WRITABLE (1 << 1)
 #define PAGE_USER (1 << 2)
-/*we don't use the rest of the flags*/
+#define PAGE_WRITE_THROUGH_CACHE  (1 << 3)
+#define PAGE_CACHE_DISABLE  (1 << 4)
 
 void init_pml4 ();
 
