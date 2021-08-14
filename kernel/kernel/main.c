708544cb405324f3f5bcfdbd0ae2b5451cfe5d0d
Jett Thompson <jett.thompson.59@gmail.com>
2021-08-14 13:30:52 +1000

    made the code use the memory map to print out how much available memory is on the system

--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -3,6 +3,7 @@
 #include <thread.h>
 #include <error.h>
 #include <time.h>
+#include <memory/map.h>
 
 void arch_init (void);
 
@@ -34,6 +35,13 @@ void main (void)
     create_thread(&thread_id, thread_start, "A");
     puts ("Thread 2:");
     putnum64(thread_id, 10);
+    puts ("Available memory:");
+    u64_t available_memory = 0;
+    memory_map_t* available_memory_map = get_available_memory();
+    for (u32_t i = 0; i < available_memory_map->number_of_blocks; i ++) {
+        available_memory += available_memory_map->blocks [i].length;
+    }
+    puthex64 (available_memory);
     __asm__ ("sti");
     putchar ('\n');
     puts ("completed initialisation");    
