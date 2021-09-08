94df52d329f8f2a056a1a5cbb45e951a0fd0af17
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-08 19:59:30 +1000

    fixed a number of bugs which were causing the os to crash so now it works! WITH LESS BSS USAGE!

--- kernel/arch/x86_64/kernel/paging/paging.c
+++ kernel/arch/x86_64/kernel/paging/paging.c
@@ -3,7 +3,7 @@
 
 page_table_entry_t* init_page_table_0_0(u16_t page_table_index, page_table_entry_t base)
 {
-    page_table_entry_t* page_table = allocate_frame();
+    page_table_entry_t* page_table = allocate_frame() * 4096;
     page_table_entry_t physical_address = base;
     for (int i = 0; i < 512; i++, physical_address+=4096){
         page_table[i] = physical_address;
@@ -18,11 +18,11 @@ page_table_entry_t* init_pdd_0_0 ()
     // identity map the first 64 megabytes
     for (i = 0; i < 32; i++) {
         pdd_0_0[i] = (page_table_entry_t)init_page_table_0_0 (i, base) | PAGE_PRESENT | PAGE_WRITABLE;
-        base += 1024 * 1024 * 2;
+        base += 4096 * 512;
     }
     // put the rest of the page tables in empty so they can be allocated later
     for (; i < 512; i++) {
-        pdd_0_0[i] = allocate_frame() * 4096 | PAGE_PRESENT | PAGE_WRITABLE;
+        pdd_0_0[i] = (allocate_frame() * 4096) | PAGE_PRESENT | PAGE_WRITABLE;
     }
     return pdd_0_0;
 }
@@ -34,7 +34,7 @@ page_table_entry_t* init_pdp0 ()
 }
 void init_pml4 ()
 {
-    page_table_entry_t* pml4 = allocate_frame();
+    page_table_entry_t* pml4 = allocate_frame() * 4096;
     pml4[0] = (page_table_entry_t)init_pdp0 () | PAGE_PRESENT | PAGE_WRITABLE;
     //recursive mapping of page tables makes them accessible at a fixed virtual address
     pml4 [511] = ((u64_t) & (pml4[0])) | PAGE_PRESENT | PAGE_WRITABLE;
