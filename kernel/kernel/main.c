8a0ef784ec1d409de23a1db98ce7f5464535f2c2
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-11 13:25:14 +1000

    made the kernel less verbose

--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -9,13 +9,7 @@ void arch_init (void);
 
 void thread_start (void* arg)
 {
-    char* char_arg = (char*)arg;
-    u64_t previous = 0;
     loop:
-    if (previous + 1000000000 <= nanotime ()) {
-        putchar (*char_arg);
-        previous = nanotime ();
-    }
     goto loop;
 }
 
@@ -30,18 +24,6 @@ void main (void)
     initialise_drivers (3);
     thread_t thread_id;
     create_thread(&thread_id, thread_start, "a");
-    puts ("Thread1:");
-    putnum64(thread_id, 10);
-    create_thread(&thread_id, thread_start, "A");
-    puts ("Thread 2:");
-    putnum64(thread_id, 10);
-    puts ("Available memory:");
-    u64_t available_memory = 0;
-    memory_map_t* available_memory_map = get_available_memory();
-    for (u32_t i = 0; i < available_memory_map->number_of_blocks; i ++) {
-        available_memory += available_memory_map->blocks [i].length;
-    }
-    puthex64 (available_memory);
     __asm__ ("sti");
     putchar ('\n');
     puts ("completed initialisation");    
