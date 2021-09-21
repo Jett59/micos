8ce1d66228ac1e9cedfbde9a434c883f55606f20
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-21 19:36:29 +1000

    made the code not crash (but not work either)

--- kernel/arch/x86_64/kernel/paging/malloc/alloc.c
+++ kernel/arch/x86_64/kernel/paging/malloc/alloc.c
@@ -10,7 +10,7 @@ void *malloc(size_t size, unsigned int alignment) {
   u8_t *working_ptr = ptr;
   u64_t first_page = ((u64_t)working_ptr + 4095) / 4096 * 4096;
   u64_t last_page = first_page + (size + 4095) / 4096 * 4096;
-  for (int i = first_page; i <= last_page; i += 4096) {
+  for (u64_t i = first_page; i <= last_page; i += 4096) {
     map_page(allocate_frame(), i >> 12, PAGE_PRESENT | PAGE_WRITABLE);
   }
   working_ptr = ptr;
@@ -26,7 +26,7 @@ void *malloc_uncacheable(size_t size, unsigned int alignment) {
   u8_t *working_ptr = ptr;
   u64_t first_page = ((u64_t)working_ptr + 4095) / 4096 * 4096;
   u64_t last_page = first_page + (size + 4095) / 4096 * 4096;
-  for (int i = first_page; i <= last_page; i += 4096) {
+  for (u64_t i = first_page; i <= last_page; i += 4096) {
     map_page(allocate_frame(), i >> 12,
              PAGE_PRESENT | PAGE_WRITABLE | PAGE_CACHE_DISABLE);
   }
