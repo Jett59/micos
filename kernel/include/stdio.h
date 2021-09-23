efdbecdb85b8b27697bb5891c1cddf37181f39af
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-23 11:00:12 +1000

    created a function to print decimal numbers (putdec64) and also made puthex64 inline

--- kernel/include/stdio.h
+++ kernel/include/stdio.h
@@ -6,7 +6,13 @@
 int puts(char* str);
 int putchar(char c);
 
-void putnum64(u64_t num, int regex);
-void puthex64(u64_t num);
+void putnum64(u64_t num, int radix);
+
+static inline void puthex64(u64_t num) { putchar('0');
+  putchar('x');
+  putnum64(num, 16);
+}
+
+static inline void putdec64(u64_t num) { putnum64(num, 10); }
 
 #endif
