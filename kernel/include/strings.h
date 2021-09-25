26b8507878f33aa68d2a4abc604d518af5cb8df2
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-26 07:42:13 +1000

    added the function to the header file

--- kernel/include/strings.h
+++ kernel/include/strings.h
@@ -3,17 +3,18 @@
 
 #include <stdint.h>
 
-void* memset(void* s, u8_t value, size_t size);
-void* memcpy(void* src, void* dst, size_t size);
+void *memset(void *s, u8_t value, size_t size);
+void *memcpy(void *src, void *dst, size_t size);
+void *memcmp(void *str1, void *str2, size_t size);
 
-int strlen(const char* str);
-int strcpy(char* dest, const char* src);
-int strcmp(const char* a, const char* b);
-int strappend(char* str, char c);
+int strlen(const char *str);
+int strcpy(char *dest, const char *src);
+int strcmp(const char *a, const char *b);
+int strappend(char *str, char c);
 
-int strlen32(const u32_t* str);
-int strcpy32(u32_t* dest, const u32_t* src);
-int strcmp32(const u32_t* a, const u32_t* b);
-int strappend32(u32_t* str, u32_t n);
+int strlen32(const u32_t *str);
+int strcpy32(u32_t *dest, const u32_t *src);
+int strcmp32(const u32_t *a, const u32_t *b);
+int strappend32(u32_t *str, u32_t n);
 
 #endif
