7c306e928d3812f2bbed272b322739ceb93f5ba8
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-27 18:23:36 +1000

    created initramfs.h with a function to read a file from an initramfs

---
+++ kernel/include/fs/initramfs.h
@@ -0,0 +1,9 @@
+#ifndef _FS_INITRAMFS_H
+#define _FS_INITRAMFS_H  
+
+#include <stdint.h>
+
+// Gets the contents of the specified file in the initramfs, returning the size or 0 if the file could not be found, and putting a pointer to the data in data.
+size_t initramfs_read(unsigned char* initramfs, const char* file_name, unsigned char** data);
+
+#endif
