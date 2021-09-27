1e902f3c023a719251258a951d841d68dc9bca4a
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-27 18:43:47 +1000

    Implemented a function to read a file from an initramfs

--- kernel/fs/initramfs/initramfs.c
+++ kernel/fs/initramfs/initramfs.c
@@ -1,1 +1,39 @@
 #include <fs/initramfs.h>
+#include <strings.h>
+
+static inline size_t octal_to_binary(unsigned char *octal, size_t size) {
+  size_t result = 0;
+  for (size_t i = 0; i < size; i++) {
+    result *= 8;
+    result += octal[i] - '0';
+  }
+  return result;
+}
+
+size_t initramfs_read(unsigned char* initramfs, size_t initramfs_size, const char* file_name, unsigned char** data) {
+  // Preliminary checks: is this a valid tar file?
+  if (initramfs_size < 512) {
+      // Too small to be a tar file
+    return 0;
+  }
+  if (memcmp(initramfs + 257, "ustar", 5)) {
+      // No "ustar" signature found
+    return 0;
+  }
+  size_t offset = 0;
+  while (offset < initramfs_size && !memcmp(initramfs + offset + 257, "ustar", 5)) {
+    size_t file_size = octal_to_binary(initramfs + offset + 124, 11);
+    unsigned char file_type = initramfs[offset + 156];
+    if (file_type == '0' || file_type == 0) {
+        // Regular file
+        if(!strcmp(file_name, initramfs + offset)) {
+            // Found it!
+            *data = initramfs + offset + 512;
+            return file_size;
+        }
+    }
+    offset += (file_size + 512 + 511) / 512 * 512;
+  }
+  // Didn't find it.
+  return 0;
+}
