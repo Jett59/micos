b6e548c40f7eaa805a47bbee847be5ee7ad74bb2
Jett Thompson <jett.thompson.59@gmail.com>
2021-10-21 19:23:22 +1100

    fixed console.c to properly support headless machines

--- kernel/kernel/drivers/console/console.c
+++ kernel/kernel/drivers/console/console.c
@@ -1,15 +1,18 @@
 #include <display.h>
 #include <fonts/renderer.h>
 #include <malloc.h>
+#include <stdbool.h>
 #include <strings.h>
 
-#define get_start_of_line(line) \
+#define get_start_of_line(line)                                                \
   (screen_buffer + (line) * (characters_per_line + 1))
 #define get_character_at(x, y) (*(get_start_of_line(y) + (x)))
-#define set_character_at(x, y, character) \
+#define set_character_at(x, y, character)                                      \
   (*(get_start_of_line(y) + (x)) = (character))
 
 #define get_line_length(line) strlen32(get_start_of_line(line))
+
+static bool headless = false;
 
 static int current_x = 0;
 static int current_y = 0;
@@ -58,33 +61,38 @@ static inline void new_line() {
 }
 
 void console_write_char(u32_t character) {
-  if (lines == 0 || characters_per_line == 0) {
+  if (!headless && (lines == 0 || characters_per_line == 0)) {
     vidmode = *get_video_mode();
-    lines = vidmode.height / get_character_height();
-    characters_per_line = vidmode.width / (get_character_width() + 1);
-    current_x = 0;
-    current_y = 0;
-    screen_buffer =
-        malloc(lines * (characters_per_line + 1) * sizeof(u32_t));
-    *screen_buffer = 0;
-  }
-  if (character == '\n') {
-    new_line();
-  } else if (character == 0x8) {
-    if (current_x > 0) {
-      current_x--;
-      render_character(current_x, current_y, ' ', default_foreground,
-                       default_background);
-      set_character_at(current_x, current_y, 0);
+    if (vidmode.width && vidmode.height) {
+      lines = vidmode.height / get_character_height();
+      characters_per_line = vidmode.width / (get_character_width() + 1);
+      current_x = 0;
+      current_y = 0;
+      screen_buffer = malloc(lines * (characters_per_line + 1) * sizeof(u32_t));
+      *screen_buffer = 0;
+    } else {
+      headless = true;
     }
-  } else {
-    render_character(current_x, current_y, character, default_foreground,
-                     default_background);
-    set_character_at(current_x, current_y, character);
-    set_character_at(current_x + 1, current_y, 0);
-    current_x++;
-    if (current_x >= characters_per_line) {
+  }
+  if (!headless) {
+    if (character == '\n') {
       new_line();
+    } else if (character == 0x8) {
+      if (current_x > 0) {
+        current_x--;
+        render_character(current_x, current_y, ' ', default_foreground,
+                         default_background);
+        set_character_at(current_x, current_y, 0);
+      }
+    } else {
+      render_character(current_x, current_y, character, default_foreground,
+                       default_background);
+      set_character_at(current_x, current_y, character);
+      set_character_at(current_x + 1, current_y, 0);
+      current_x++;
+      if (current_x >= characters_per_line) {
+        new_line();
+      }
     }
   }
 }
