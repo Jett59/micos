7ca04492c7150699c9b48a07574da84b6ba11d5d
Jett Thompson <jett.thompson.59@gmail.com>
2021-10-23 12:38:28 +1100

    made the test script use grep to check if the log file contains the correct messages

--- debugging/qemu-test.sh
+++ debugging/qemu-test.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
-make clean
-make iso
+set +e
+
 timeout 20s qemu-system-x86_64 -cdrom build/Micos.iso -serial stdio -display none > qemu-test.log
+
+set -e
+
+cat qemu-test.log | grep Welcome
+cat qemu-test.log | grep Done
