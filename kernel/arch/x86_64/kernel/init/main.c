5c9fbeaf032c2575d32933e24752b3b326fa45fa
Jett Thompson <jett.thompson.59@gmail.com>
2021-11-08 16:56:13 +1100

    made the tss get initialized during early kernel setup

--- kernel/arch/x86_64/kernel/init/main.c
+++ kernel/arch/x86_64/kernel/init/main.c
@@ -1,6 +1,7 @@
 #include <interrupts.h>
 #include <pics.h>
 #include <stdio.h>
+#include <tss.h>
 
 extern volatile u64_t number_of_debug_interrupts;
 
@@ -63,13 +64,13 @@ void init_interrupts() {
       STANDARD_INTERRUPT((u64_t)&security_exception_handler);
   idt_pointer.limit = sizeof(idt) - 1;
   idt_pointer.ptr = (u64_t)idt;
-  __asm__ volatile ("movabsq %0, %%rax;"
-          "lidt (%%rax);"
-          :
-          : "i"(&idt_pointer)
-          : "rax");
+  __asm__ volatile("movabsq %0, %%rax;"
+                   "lidt (%%rax);"
+                   :
+                   : "i"(&idt_pointer)
+                   : "rax");
   u64_t preserve = number_of_debug_interrupts;
-  __asm__ volatile ("int %0" : : "i"(INTERRUPT_DEBUG));
+  __asm__ volatile("int %0" : : "i"(INTERRUPT_DEBUG));
   if (number_of_debug_interrupts <= preserve) {
     puts("interrupt didn't run");
   die:
@@ -84,6 +85,7 @@ void arch_init() {
   scan_mbi();
   init_interrupts();
   configure_pics();
+  setup_tss();
   return;
 }
 
--- kernel/arch/x86_64/kernel/task/tss.c
+++ kernel/arch/x86_64/kernel/task/tss.c
@@ -1,3 +1,10 @@
 #include <tss.h>
 
 tss_t tss;
+
+u8_t rsp0[8192];
+
+void setup_tss() {
+  tss.rsp0 = rsp0;
+  tss.io_port_base_offset = 104;
+}
