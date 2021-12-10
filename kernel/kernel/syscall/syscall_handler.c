56c06c0b3f53692a65e7178dfdd61c5545f790d1
Jett Thompson <jett.thompson.59@gmail.com>
2021-12-11 04:24:18 +1100

    created an enum for storing syscalls, added support for the message_post, message_pending and message_get functions through syscalls and made the init service use these to send an opcode to the main kernel init thread

--- kernel/kernel/syscall/syscall_handler.c
+++ kernel/kernel/syscall/syscall_handler.c
@@ -1,19 +1,32 @@
+#include <message.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <syscalls.h>
 
-void syscall_handler(u32_t code, ...) {
-    va_list varargs;
-    va_start(varargs, code);
-    switch (code) {
-        case 101: {
-            // Debug trace
-            puts("Debug trace");
-            break;
-        }
-        default:
-          puts("Unknown syscall number:");
-          putdec64(code);
-    }
-    va_end(varargs);
+syscall_result_t syscall_handler(syscall_code_t code, ...) {
+  va_list varargs;
+  va_start(varargs, code);
+  syscall_result_t result = {0};
+  switch (code) {
+      case SYSCALL_MESSAGE_POST: {
+        result.message_status =
+            message_post(va_arg(varargs, message_header_t),
+                         va_arg(varargs, message_payload_t));
+        break;
+      }
+  case SYSCALL_MESSAGE_PENDING: {
+    result.u16 = message_pending();
+    break;
+  }
+  case SYSCALL_MESSAGE_GET: {
+    message_get(va_arg(varargs, message_t*));
+    break;
+  }
+  default:
+    puts("Unknown syscall number:");
+    putdec64(code);
+  }
+  va_end(varargs);
+  return result;
 }
