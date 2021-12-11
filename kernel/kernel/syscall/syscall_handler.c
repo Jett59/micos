6d62326d38540b4e102091dbe2d791b610c57a1f
Jett Thompson <jett.thompson.59@gmail.com>
2021-12-11 16:34:14 +1100

    implemented some security with the pointer passed to message_get so it is not possible to pass a pointer to some kernel structure

--- kernel/kernel/syscall/syscall_handler.c
+++ kernel/kernel/syscall/syscall_handler.c
@@ -1,4 +1,5 @@
 #include <message.h>
+#include <ptrrange.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -20,7 +21,12 @@ syscall_result_t syscall_handler(syscall_code_t code, ...) {
     break;
   }
   case SYSCALL_MESSAGE_GET: {
-    message_get(va_arg(varargs, message_t*));
+    message_t* destination_pointer = va_arg(varargs, message_t*);
+    if (IS_USER_MODE_POINTER(destination_pointer)) { // Don't overwrite kernel memory
+    message_get(destination_pointer);
+    }else {
+      puts("The application just tried to compromise the kernel!");
+    }
     break;
   }
   default:
