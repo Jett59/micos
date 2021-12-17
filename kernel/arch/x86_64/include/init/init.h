a582a71bb6155c83a009c88bf9980ef4375cc0aa
Jett Thompson <jett.thompson.59@gmail.com>
2021-12-18 08:22:09 +1100

    removed the <init/> header files as well as the memory/pointers.h and core/kernel.c files

--- kernel/arch/x86_64/include/init/init.h
+++
@@ -1,6 +0,0 @@
-#ifndef _INIT_INIT_H
-#define _INIT_INIT_H
-
-void init_end();
-
-#endif
--- kernel/core/kernel.c
+++
@@ -1,6 +0,0 @@
-#include <memory/pointers.h>
-
-#ifdef __x86_64
-void *get_pointer(size_t address) { return (void *)address; }
-size_t get_linear_address(void *ptr) { return (size_t)ptr; }
-#endif
